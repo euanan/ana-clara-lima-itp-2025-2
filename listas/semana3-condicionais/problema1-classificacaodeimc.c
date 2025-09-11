262cdf4e9a9886c8e26900bfa2f5ad38d29888d3
Ana Lima <analima6728@gmail.com>
2025-09-11 01:36:41 -0300

    problema 1 - semana 3

---
+++ listas/semana3-condicionais/problema1-classificacaodeimc.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    // Configurando a localização para português
+    setlocale(LC_ALL, "Portuguese");
+    
+    float peso, altura, imc;
+    
+    //Solicitar o peso
+    printf("Digite o seu peso, em kg: ");
+    scanf("%f", &peso);
+    
+    //Solicitar a altura
+    printf("Digite a sua altura, em metros: ");
+    scanf("%f", &altura);
+    
+    // Calcular o IMC
+    imc = peso / (altura * altura);
+    
+    // Exibir o resultado
+    printf("Seu IMC é: %.2f\n", imc);
+
+    if (imc < 18.5) {
+        printf("Você está abaixo do peso.\n");
+    }
+    else if (18.5 < imc && imc < 24.9) {
+        printf("Você possui o peso normal.\n");
+    }
+    else if (25.0 < imc && imc < 29.9) {
+        printf("Você possui sobrepeso.\n");
+    }
+    else if (30.0 <= imc) {
+        printf("Você possui obesidade.\n");
+    }
+
+
+    return 0;
+}
