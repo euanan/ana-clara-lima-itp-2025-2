ce33b98165169458c7b7d48d78943b43f984a75b
Ana Lima <analima6728@gmail.com>
2025-10-30 18:16:26 -0300

    problema 2 semana 8

--- listas/semana8-repeticoes-aninhadas/problema2.c
+++ listas/semana8-repeticoes-aninhadas/problema2.c
@@ -2,35 +2,56 @@
 #include <string.h>
 
 int main() {
-    int quant_chuvas[12], quant_chuva_ordenadas[13][4], temp_chuva, temp_chuvas;
-    char meses[][9] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
-    char temp_mes[9];
+    // Variáveis e vetores
+    int quant_chuvas[12], temp_chuva;
+    char meses[][10] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+    char temp_mes[10];
 
+    // Variável que armazena a posição do amior elemento:
+    int indice_maior;
+
+
+    // Recebendo as quantidades de chuva de cada um dos meses:
     for (int i = 0; i < 12; i++)
     {
         printf("Digite a quantidade de chuvas no mês de %s: ", meses[i]);
         scanf("%d", &quant_chuvas[i]);
     }
+
+    //Ordenação decrescente:
     for (int j = 0; j < 12 - 1; j++)
     {
-        for (int k = 0; k < 12 - 1 - j; k++)
-        {
-           if (quant_chuvas[k] < quant_chuvas[k + 1])
-           {
-            temp_chuva = quant_chuvas[k];
-            quant_chuvas[k] = quant_chuvas[k + 1];
-            quant_chuvas[k + 1] = temp_chuva;
+        indice_maior = j; //Assumimos que o maior elemento na posição atual 'j'
 
-            strcpy(temp_mes, meses[k]); 
-            strcpy(meses[k], meses[k + 1]);
-            strcpy(meses[k + 1], temp_mes);
+        // Laço que busca o maior elemento do resto do vetor (de j+1 até o fim):
+        for (int k = j + 1; k < 12; k++)
+        {
+            //Condição para encontrar um valor maior que o atual (índice_maior):
+           if (quant_chuvas[k] > quant_chuvas[indice_maior])
+           {
+            indice_maior = k;
            }
         }  
+        // Se indice_maior for diferente de j, significa que encontramos um elemento maior
+        if (indice_maior != j) {
+
+            // Troca coordenada de inteiros (entre j e indice):
+            temp_chuva = quant_chuvas[j];
+            quant_chuvas[j] = quant_chuvas[indice_maior];
+            quant_chuvas[indice_maior] = temp_chuva;
+
+            // Troca coordenada de strings(entre j e indice_maior):
+            strcpy(temp_mes, meses[j]); 
+            strcpy(meses[j], meses[indice_maior]);
+            strcpy(meses[indice_maior], temp_mes);
+        }
     }
+    // Imprimindo em ordem decrescente as quantidades de chuva e seus respectivos meses:
     for (int l = 0; l < 12; l++) 
     {
         printf("%s %d\n", meses[l], quant_chuvas[l]);
     }
 
     return 0;
+    
 }
