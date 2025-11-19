92e9fb7f188d6179f84e4a05d6c755b2120c355f
Ana Lima <analima6728@gmail.com>
2025-11-18 21:56:15 -0300

    problema 2 semana 10
    sem todos os comentários

---
+++ listas/semana10-ponteiros-alocacao/problema2.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n, valor;
+    int qtd = 0;
+
+    int * buscaNoVetor(int *v, int n, int valor, int *maior, int *qtd); //Protótipo
+
+    printf("Digite a quantidade de números: \n");
+    scanf("%d", &n);
+
+    // Alocação dinâmica do vetor de entrada:
+    int *v = (int *) malloc(n * sizeof(int));
+
+    // Validação básica de alocação (boa prática)
+    if (v == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return 1;
+    }
+
+    // Recebendo os elementos:
+    for (int i = 0; i < n; i++)
+    {
+        printf("Digite o %d° número(índice %d): \n", i+1, i);
+        scanf("%d", &v[i]);
+    }
+
+    printf("Digite o número que você deseja saber a quantidade de ocorrências:\n");
+    scanf("%d", &valor);
+
+    int inutil;
+    int *resultado_indices = buscaNoVetor(v, n, valor, &inutil, &qtd);
+
+    if (resultado_indices == NULL)
+    {
+        printf("Nenhuma ocorrencia.\n");
+    } else {
+        printf("%d\n", qtd);
+        for (int i = 0; i < qtd; i++)
+        {
+            printf("%d ", resultado_indices[i]);
+        }
+        printf("\n");
+
+        free(resultado_indices); //Libera a memória alocada na função.
+    }
+    free(v); //Libera memória do vetor de entrada
+
+    return 0;
+}
+
+int * buscaNoVetor(int *v, int n, int valor, int *maior, int *qtd) {
+    int cont = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] == valor)
+        {
+            cont++;
+        }   
+    } // Varre de 1 até n para contar as ocorrências
+
+    *qtd = cont; // Atualiza o valor apontado por qtd na main
+
+    if (cont == 0)
+    {
+        return NULL;
+    }
+
+    int *indices = (int *) malloc(cont * sizeof(int)); //Aloca memória apenas para a quantidade de índices encontrados
+    
+    int j = 0; // Índice para o novo vetor
+    for (int i = 0; i < n; i++)
+    {
+        if (v[i] == valor)
+        {
+            indices[j] = i;
+            j++;
+        }
+    } // Varre até n novamente para salvar as posições
+    
+    return indices;
+}
