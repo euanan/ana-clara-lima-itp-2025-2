0e47aa998bdf615a9f0b243b1d7d58e18031a41c
Ana Lima <analima6728@gmail.com>
2025-11-19 19:29:58 -0300

    problema 3 semana 10
    sem todos os comentários

---
+++ listas/semana10-ponteiros-alocacao/problema3.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *misturar(char *str1, char *str2); // Protótipo
+
+int main() {
+    char str1[100];
+    char str2[100];
+
+    printf("Digite a primeira string:\n");
+    fgets(str1, 100, stdin);
+
+    printf("Digite a segunda string:\n");
+    fgets(str2, 100, stdin);
+
+    // Removendo o caractere de nova linha '\n' lido por fgets
+    str1[strcspn(str1, "\n")] = 0;
+    str2[strcspn(str2, "\n")] = 0;
+
+    char *str_misturada = misturar(str1, str2);
+
+    if (str_misturada != NULL)
+    {
+        printf("%s", str_misturada);
+        free(str_misturada);
+        str_misturada = NULL;
+    } else {
+        printf("Erro na alocção de memória.\n");
+    }
+   
+    return 0;
+}
+
+char *misturar(char *str1, char *str2) {
+    int tamanho_str1 = strlen(str1);
+    int tamanho_str2 = strlen(str2);
+    int tamanho_total = tamanho_str1 + tamanho_str2 + 1; // +1 para "\0"
+
+    char *str3 = (char *)malloc(tamanho_total * sizeof(char)); // Alocação dinâmica
+    if (!str3) return NULL;
+
+
+    int cont = 0;
+    int i = 0;
+
+    while (i < tamanho_str1 && i < tamanho_str2) {
+        str3[cont++] = str1[i];
+        str3[cont++] = str2[i];
+        i++;
+    } // Alterna enquanto houver letras nas duas
+
+    // Copiando o restante da maior:
+    while (i < tamanho_str1) {
+        str3[cont++] = str1[i++];
+    } 
+
+    while (i < tamanho_str2) {
+        str3[cont++] = str2[i++];
+    }
+
+    str3[cont] = '\0';
+    
+    return str3;
+}
